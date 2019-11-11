11fdd2a0b1b5b99f6ef037a077158c5e2d48fd75
Emma Ilkka <emma.ilkka@gmail.com>
2019-11-11 12:31:04 +0200

    korjattiin checkstyle-rike

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -3,8 +3,8 @@ package ohtu.ohtuvarasto;
 public class Varasto {
 
     // --- piilotettu tietorakenteen toteutus: ---
-        private double tilavuus;  // paljonko varastoon mahtuu,  > 0
-        private double saldo;     // paljonko varastossa on nyt, >= 0
+    private double tilavuus;  // paljonko varastoon mahtuu,  > 0
+    private double saldo;     // paljonko varastossa on nyt, >= 0
 
     // --- konstruktorit: ---
     public Varasto(double tilavuus) {  // tilavuus on annettava
